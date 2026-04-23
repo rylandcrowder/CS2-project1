8a90ff1398ef8e4de0b481c803911974129c30ea
Ryland Crowder <rylandcrowder@gmail.com>
2026-04-23 17:00:19 -0500

    almost done

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from logic import *
 
 def main():
+    """
+    This is the main function that runs the application. It creates an instance 
+    of the Logic class, shows the GUI, and starts the event loop.
+    """
     app = QApplication([])
     gui = Logic()
     gui.show()
